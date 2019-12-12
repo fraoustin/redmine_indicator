d7b9f3aa2ab9d0c72f844e572301765c37c95ad6
fraoustin <fraoustin@gmail.com>
2019-12-12 16:50:07 +0100

    <feat> chartjs: init

--- assets/javascripts/indicator.js
+++ assets/javascripts/indicator.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
     console.log("indicator");
+    if (document.querySelectorAll('.chartjs').length > 0) {
+        var script = document.createElement("script")
+        script.src = '/plugin_assets/redmine_indicator/javascripts/Chart.min.js'
+        script.onload = function(){
+            Array.from(document.querySelectorAll('.chartsjs')).forEach(elt => {
+            var code = elt.innerText;
+            //chart = ...
+            //elt.parentNode.insertBefore(chart, elt);
+            });
+        }
+        document.head.appendChild(script)
+    }
+
+
+
 });
  
 
