d318c01718757cf19b76cba40eb1c70f5db11e85
fraoustin <fraoustin@gmail.com>
2019-12-13 16:01:34 +0100

    <fix> block: change name

--- assets/javascripts/indicator.js
+++ assets/javascripts/indicator.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
         script.src = '/plugin_assets/redmine_indicator/javascripts/Chart.min.js'
         script.onload = function(){
             Array.from(document.querySelectorAll('.chartsjs')).forEach(elt => {
-            var code = elt.innerText;
+            var code = JSON.parse(elt.innerText);
+            console.log(code);
             //chart = ...
             //elt.parentNode.insertBefore(chart, elt);
             });
