f9ce0e03b3a11490bbc1a94e6b7b5e105dbf9a51
fraoustin <fraoustin@gmail.com>
2019-12-17 17:55:01 +0100

    <feat> blocks: add status, tracker and leftIndicatorTop and Bottom

--- assets/javascripts/indicator.js
+++ assets/javascripts/indicator.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     console.log("indicator");
-    if (document.querySelectorAll('#leftIndicator').length > 0) {
-        src = document.querySelectorAll('#leftIndicator')[0]
+    if (document.querySelectorAll('#leftIndicatorTop').length > 0) {
+        src = document.querySelectorAll('#leftIndicatorTop')[0]
         dst = document.querySelectorAll('.splitcontentleft')[0]
         before = document.querySelectorAll('.splitcontentleft div')[0]
         dst.insertBefore(src, before)
