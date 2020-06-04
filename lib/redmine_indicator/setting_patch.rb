062b4bdb84527782c07472149b5b0bc464c1b2be
user <lucky.vb@gmail.com>
2020-06-04 16:17:14 +0300

    Hook adding js+css to head problem

--- lib/redmine_indicator/setting_patch.rb
+++
@@ -1,5 +0,0 @@
-module RedmineIndicator
-  #::AVAILABLE_REPORTS = ['summary','opened_closed_today','opened_closed','opened_closed_10days','opened_closed_10weeks','spent_time_10days','spent_time_10weeks','by_priority','by_priority_all','by_status','by_status_all','by_tracker','by_tracker_all']
-  module SettingPatch
-  end
-end
