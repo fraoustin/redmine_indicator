be1da50904f2d7438afb8ce5b9e3df019c7a235e
fraoustin <fraoustin@gmail.com>
2025-12-24 18:31:24 +0100

    <fix> upgrade Redmine6

---
+++ assets/config/manifest.js
@@ -0,0 +1,4 @@
+//= link redmine_indicator/indicator.js
+//= link redmine_indicator/indicator.css
+//= link redmine_indicator/Chart.min.css
+//= link redmine_indicator/Chart.min.js
--- lib/redmine_indicator.rb
+++ lib/redmine_indicator.rb
@@ -19,6 +19,7 @@ module RedmineIndicator
     class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context = {})
         return javascript_include_tag('indicator.js', :plugin => 'redmine_indicator') +
+          javascript_include_tag('Chart.min.js', :plugin => 'redmine_indicator') +
           stylesheet_link_tag("indicator.css", :plugin => "redmine_indicator", :media => "all")+
           stylesheet_link_tag("Chart.min.css", :plugin => "redmine_indicator", :media => "all")
       end
